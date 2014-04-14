47cad8f7cc09add4f27f30e6618170c35e4af694
geghetsik <geghetsik@ubuntu.(none)>
2014-04-14 22:13:56 +0400

    first commit

---
+++ src/AttributeAxis.hpp
@@ -0,0 +1,21 @@
+#ifndef ATTRIBUTE_AXIS_HPP
+#define ATTRIBUTE_AXIS_HPP
+
+template <class T>
+
+class DataComponent {
+	public:
+
+	private:
+		bool 				isIndependent;
+		bool 				isTarget;
+		bool				isConstraint;
+		Point				
+		std::set<DataComponents>	attributeValues.
+	
+};
+
+
+
+
+#endif  // ATTRIBUTE_AXIS_HPP
---
+++ src/DataComponent.hpp
@@ -0,0 +1,19 @@
+#ifndef DATA_COMPONENT_HPP
+#define DATA_COMPONENT_HPP
+
+template <class T>
+
+class DataComponent {
+public:
+	
+private:
+	AttributeAxis<T>* attributeAxis;
+	T 		  value;
+	DataEntry*	  dataEntry;	
+	
+};
+
+
+
+
+#endif  // DATA_COMPONENT_HPP
